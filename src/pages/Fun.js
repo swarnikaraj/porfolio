88e02cb8a383bb7921e762cee24b8c9383eb1987
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-04-01 22:34:06 +0530

    border position switched

--- src/pages/Fun.js
+++ src/pages/Fun.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({ title, description, link }) => {
   return (
-    <div className=" p-4 border border-solid border-[#FC6736] border-b-0 border-r-0 rounded-lg">
+    <div className=" p-4 border border-solid border-[#FC6736] border-t-0 border-l-0 rounded-lg">
       <h3 className="text-xl font-semibold mb-1 text-gray-500">{title}</h3>
       <p className="text-gray-700 mb-2">{description}</p>
       <a href={link} className="text-teal-600 hover:underline">
