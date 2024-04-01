002b41719bec8bb7ce01806843b8176516b59e36
Swarnnika Raj Singh <swarnikarajsingh@gmail.com>
2024-04-01 13:34:51 +0530

    text aligned

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,26 +1,28 @@
 import Image from "next/image";
 const Profile = () => {
   return (
-    <div className="flex h-full overflow-y-auto lg:w-1/3 w-full mid:w-1/3 flex-col items-center px-2 border-blue-800  border-l border-t border-b ">
-      <p className="py-2 text-2xl uppercase">Profile</p>
+    <div className="flex h-full overflow-y-auto lg:w-1/3 w-full mid:w-1/3 flex-col items-center px-2 border-blue-800 border-l border-t border-b">
+      <p className="py-2 text-2xl uppercase font-bold text-blue-800">Profile</p>
       <div className="relative">
         <Image
           src="https://media.licdn.com/dms/image/D5603AQHm5qVEZ2seeA/profile-displayphoto-shrink_800_800/0/1711647686431?e=1717632000&v=beta&t=BdU-0WhUXWPPM1LV4F33B-5ZhAEnoId9vSXNs43UFSY"
+          alt="Profile Picture"
+          className="rounded-full shadow-lg border-4 border-blue-800 hover:shadow-md"
           width={250}
           height={250}
-          alt="Picture of the author"
-          className="rounded-full shadow shadow-gray-300 border-4 border-blue-800 hover:shadow-sm"
         />
       </div>
-      <div className="text-blue-800 font-bold ">--Hello</div>
-      <p className=" font-mono py-1 text-xl">
+      <div className="text-blue-800 font-bold text-lg">Hello!</div>
+      <p className="font-mono py-1 text-xl">
         <span>I&apos;m</span>{" "}
-        <span className="text-blue-800 font-bold ">S</span>warnnika{" "}
-        <span className="text-blue-800 font-bold">R</span>aj{" "}
-        <span className="text-blue-800 font-bold">S</span>ingh
+        <span className="text-blue-800 font-bold">Swarnnika</span>{" "}
+        <span className="text-blue-800 font-bold">Raj</span>{" "}
+        <span className="text-blue-800 font-bold">Singh</span>
       </p>
-      <p className=" font-mono py-2">Fullstack Developer</p>
-      <div className="font-mono max-w-60 py-2">
+      <p className="font-mono py-2 text-lg text-blue-800">
+        Fullstack Developer
+      </p>
+      <div className="font-mono max-w-md py-2 text-lg text-center text-gray-700">
         Passionate full-stack developer skilled in end-to-end project
         development and deployment. Known for agility with new technologies and
         innovative problem-solving.
